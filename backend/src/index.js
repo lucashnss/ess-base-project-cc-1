dd738ed16740f8ea818db165ab01b54e5f78734b
João Reis <reis.joaoantonio37@gmail.com>
2025-02-17 01:47:00 -0300

    initial commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,5 @@
-<<<<<<< HEAD
-import express from 'express'
-const app = express();
-
-app.use(express.json())
-
-import filmesRoutes from './routes/filmes.routes.js'
-
-app.use('/filmes', filmesRoutes)
 
 
-app.listen(4000);
-=======
 const app = require('./app');
 const logger = require('./logger');
 const Env = require('./env');
@@ -18,4 +7,4 @@ const Env = require('./env');
 app.listen(Env.PORT, () => {
   logger.info(`Server started on http://localhost:${Env.PORT}/api`);
 });
->>>>>>> 926779ac140e4ca9fa17167edb2fa6f2c725bf40
+
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,7 +1,9 @@
 module.exports = (app) => {
   const historyRoutes = require('./history.routes');
   const videoRoutes = require('./video.routes');
+  const filmeRoutes = require('./filmes.routes')
 
   app.use('/api', historyRoutes);
   app.use('/api', videoRoutes);
+  app.use('/filmes', filmeRoutes)
 };
